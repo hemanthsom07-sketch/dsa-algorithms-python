8fc92c2ffdb9c2ff01cb7ffd3b556864cef2b3c3
hemanthsom07-sketch <hemanth.som07@gmail.com>
2026-03-15 21:37:47 +0530

    Update bubble_sort.py

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,1 +1,23 @@
+# Algorithm: Bubble Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+
+# User input
+arr = list(map(int, input("Enter array elements separated by space: ").split()))
+
+# Function call
+sorted_array = bubble_sort(arr)
+
+# Output
+print("Sorted array:", sorted_array)
